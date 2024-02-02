5d899f3e8269daccf0a0a97ab7895a9d952fd0bf
Rendra <133106820+pandhu-rendra@users.noreply.github.com>
2024-02-02 21:25:25 +0700

    ngedit doang

--- index.js
+++ index.js
@@ -1,123 +1,77 @@
-const apiKey = 'd59d68d3e21f7c2e9f91863d2b1a5f53';
-const locButton = document.querySelector('.loc-button');
-const todayInfo = document.querySelector('.today-info');
-const todayWeatherIcon = document.querySelector('.today-weather i');
-const todayTemp = document.querySelector('.weather-temp');
-const daysList = document.querySelector('.days-list');
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
-// Mapping of weather condition codes to icon class names (Depending on Openweather Api Response)
-const weatherIconMap = {
-    '01d': 'sun',
-    '01n': 'moon',
-    '02d': 'sun',
-    '02n': 'moon',
-    '03d': 'cloud',
-    '03n': 'cloud',
-    '04d': 'cloud',
-    '04n': 'cloud',
-    '09d': 'cloud-rain',
-    '09n': 'cloud-rain',
-    '10d': 'cloud-rain',
-    '10n': 'cloud-rain',
-    '11d': 'cloud-lightning',
-    '11n': 'cloud-lightning',
-    '13d': 'cloud-snow',
-    '13n': 'cloud-snow',
-    '50d': 'water',
-    '50n': 'water'
-};
+search.addEventListener('click', () => {
 
-function fetchWeatherData(location) {
-    // Construct the API url with the location and api key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
+    const APIKey = 'd59d68d3e21f7c2e9f91863d2b1a5f53';
+    const city = document.querySelector('.search-box input').value;
 
-    // Fetch weather data from api
-    fetch(apiUrl).then(response => response.json()).then(data => {
-        // Update todays info
-        const todayWeather = data.list[0].weather[0].description;
-        const todayTemperature = `${Math.round(data.list[0].main.temp)}°C`;
-        const todayWeatherIconCode = data.list[0].weather[0].icon;
+    if (city === '')
+        return;
 
-        todayInfo.querySelector('h2').textContent = new Date().toLocaleDateString('en', { weekday: 'long' });
-        todayInfo.querySelector('span').textContent = new Date().toLocaleDateString('en', { day: 'numeric', month: 'long', year: 'numeric' });
-        todayWeatherIcon.className = `bx bx-${weatherIconMap[todayWeatherIconCode]}`;
-        todayTemp.textContent = todayTemperature;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
 
-        // Update location and weather description in the "left-info" section
-        const locationElement = document.querySelector('.today-info > div > span');
-        locationElement.textContent = `${data.city.name}, ${data.city.country}`;
-
-        const weatherDescriptionElement = document.querySelector('.today-weather > h3');
-        weatherDescriptionElement.textContent = todayWeather;
-
-        // Update todays info in the "day-info" section
-        const todayPrecipitation = `${data.list[0].pop}%`;
-        const todayHumidity = `${data.list[0].main.humidity}%`;
-        const todayWindSpeed = `${data.list[0].wind.speed} km/h`;
-
-        const dayInfoContainer = document.querySelector('.day-info');
-        dayInfoContainer.innerHTML = `
-
-            <div>
-                <span class="title">PRECIPITATION</span>
-                <span class="value">${todayPrecipitation}</span>
-            </div>
-            <div>
-                <span class="title">HUMIDITY</span>
-                <span class="value">${todayHumidity}</span>
-            </div>
-            <div>
-                <span class="title">WIND SPEED</span>
-                <span class="value">${todayWindSpeed}</span>
-            </div>
-
-        `;
-
-        // Update next 4 days weather
-        const today = new Date();
-        const nextDaysData = data.list.slice(1);
-
-        const uniqueDays = new Set();
-        let count = 0;
-        daysList.innerHTML = '';
-        for (const dayData of nextDaysData) {
-            const forecastDate = new Date(dayData.dt_txt);
-            const dayAbbreviation = forecastDate.toLocaleDateString('en', { weekday: 'short' });
-            const dayTemp = `${Math.round(dayData.main.temp)}°C`;
-            const iconCode = dayData.weather[0].icon;
-
-            // Ensure the day isn't duplicate and today
-            if (!uniqueDays.has(dayAbbreviation) && forecastDate.getDate() !== today.getDate()) {
-                uniqueDays.add(dayAbbreviation);
-                daysList.innerHTML += `
-                
-                    <li>
-                        <i class='bx bx-${weatherIconMap[iconCode]}'></i>
-                        <span>${dayAbbreviation}</span>
-                        <span class="day-temp">${dayTemp}</span>
-                    </li>
-
-                `;
-                count++;
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
             }
 
-            // Stop after getting 4 distinct days
-            if (count === 4) break;
-        }
-    }).catch(error => {
-        alert(`Error fetching weather data: ${error} (Api Error)`);
-    });
-}
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
 
-// Fetch weather data on document load for default location (Germany)
-document.addEventListener('DOMContentLoaded', () => {
-    const defaultLocation = 'Germany';
-    fetchWeatherData(defaultLocation);
-});
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
 
-locButton.addEventListener('click', () => {
-    const location = prompt('Enter a location :');
-    if (!location) return;
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
 
-    fetchWeatherData(location);
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+
+        });
+
+
 });
